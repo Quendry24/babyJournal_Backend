a9708392b240b7ef74240a168c1c28b320e78d27
Melissa <melissa.faliero@live.fr>
2026-03-05 17:42:31 +0100

    route parent

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-require("dotenv");
+require("dotenv").config();
+require("./models/connection");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+//var usersRouter = require("./routes/users");
+var parentsRouter = require("./routes/parents");
 
 var app = express();
 
@@ -19,6 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+//app.use("/users", usersRouter);
+app.use("/parents", parentsRouter);
 
 module.exports = app;
